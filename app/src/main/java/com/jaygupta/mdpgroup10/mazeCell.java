dfb050d8bcbfa78b332ef9b98a60c9a9dfd0ec0c
Jay Gupta <jay002@e.ntu.edu.sg>
2021-01-23 01:32:39 +0800

    Added Basic Maze Layout

---
+++ app/src/main/java/com/jaygupta/mdpgroup10/mazeCell.java
@@ -0,0 +1,17 @@
+package com.jaygupta.mdpgroup10;
+
+public class mazeCell {
+    private String cellName;
+
+    public mazeCell(String cellName) {
+        this.cellName = cellName;
+    }
+
+    public String getCellName() {
+        return cellName;
+    }
+
+    public void setCellName(String cellName) {
+        this.cellName = cellName;
+    }
+}
