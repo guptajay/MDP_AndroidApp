9f112e12b09d3ffd4beac707e17085e63be3a4d9
Jay Gupta <jay002@e.ntu.edu.sg>
2021-01-23 12:54:15 +0800

    Added a bot the maze + UI Improvements (#2)

--- app/src/main/java/com/jaygupta/mdpgroup10/mazeCell.java
+++ app/src/main/java/com/jaygupta/mdpgroup10/mazeCell.java
@@ -2,9 +2,19 @@ package com.jaygupta.mdpgroup10;
 
 public class mazeCell {
     private String cellName;
+    private String bgColor;
 
-    public mazeCell(String cellName) {
+    public mazeCell(String cellName, String bgColor) {
         this.cellName = cellName;
+        this.bgColor = bgColor;
+    }
+
+    public String getBgColor() {
+        return bgColor;
+    }
+
+    public void setBgColor(String bgColor) {
+        this.bgColor = bgColor;
     }
 
     public String getCellName() {
